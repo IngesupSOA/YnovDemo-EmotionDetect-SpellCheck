91d99fcc4b5bad56c2ba70dede23ff93024a5dea
lnans <nans.laupretre@gmail.com>
2017-02-16 10:08:48 +0100

    Front

--- WebDevice/Controllers/HomeController.cs
+++ WebDevice/Controllers/HomeController.cs
@@ -172,14 +172,14 @@ namespace WebDevice.Controllers
         {
             var corrected = await CorrectText(txt);
 
-            /*APIModels check = JsonConvert.DeserializeObject<APIModels>(corrected);
+            APIModels check = JsonConvert.DeserializeObject<APIModels>(corrected);
             string correctedText = txt;
 
             foreach (FlaggedToken correction in check.flaggedTokens)
             {
                 correctedText = correctedText.Replace(correction.token, correction.suggestions.ToList()[0].suggestion);
                 Console.WriteLine(correction.suggestions.ToList()[0].suggestion);
-            }*/
+            }
 
             //var commentString = JsonConvert.SerializeObject(commentDataPoint);
             //var message = new Microsoft.Azure.Devices.Client.Message(Encoding.ASCII.GetBytes(commentString));
@@ -189,10 +189,10 @@ namespace WebDevice.Controllers
             //await deviceClient.SendEventAsync(message);
 
             Response.StatusCode = 200; // OK = 200
-            //return correctedText;
+            return correctedText;
             //return Json(check, JsonRequestBehavior.AllowGet);
             //return JsonConvert.SerializeObject(check.flaggedTokens);
-            return corrected;
+            //return corrected;
         }
 
         static async Task<string> CorrectText(string txt)
